df215d33d9aa1d83eba2ec76111a6df6724c8ef3
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-07-18 11:26:45 -0500

    first commit for structure

---
+++ launch.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Crear entorno virtual si no existe
+IF NOT EXIST "env" (
+    echo Creando entorno virtual...
+    python -m venv env
+)
+
+REM Activar entorno virtual
+call env\Scripts\activate
+
+REM Instalar requerimientos si existen
+IF EXIST "requirements.txt" (
+    echo Instalando dependencias...
+    pip install -r requirements.txt
+)
+
+echo === Entorno listo.
+REM pero si desea mantener la consola abierta para que siga dentro del venv
+cmd /k
